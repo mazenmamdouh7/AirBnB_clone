140a9a13d220e4411077b3dd7d8aa941e4402d76
root <mazenmamdouh7>
2024-07-18 18:26:32 -0700

    Testing

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,3 +4,13 @@
 import unittest
 from models.base_model import BaseModel
 from time import sleep
+
+class TestBaseModel(unittest.TestCase):
+    """Class for testing methods."""
+    def test_id(unittest.TestCase):
+        """Method for testing id"""
+        base_model_instance = BaseModel()
+        self.assertEqual(len(base_model_instance.id), 36)
+        self.assertTrue(isinstance(base_model_instance.id, str))
+
+    def test_
