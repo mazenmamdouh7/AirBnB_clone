406c8c90d0632e7e4c00547cc928a0b2853bb4ff
root <mazenmamdouh7>
2024-07-18 20:15:38 -0700

    AirBnB

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,10 +7,26 @@ from time import sleep
 
 class TestBaseModel(unittest.TestCase):
     """Class for testing methods."""
-    def test_id(unittest.TestCase):
+    def test_id(self):
         """Method for testing id"""
         base_model_instance = BaseModel()
         self.assertEqual(len(base_model_instance.id), 36)
         self.assertTrue(isinstance(base_model_instance.id, str))
 
-    def test_
+    def test_created_at(self):
+        """Test for created at type."""
+        base_model_instance = BaseModel()
+        self.assertEqual(type(base_model_instance.created_at), datetime)
+
+    def test_updated_at(self):
+        """Test for updated at type."""
+        base_model_instance = BaseModel()
+        self.assertEqual(type(base_model_instance.updated_at), datetime)
+
+    def test_save(self):
+        """Test for save updated at"""
+        base_model_instance = BaseModel()
+        old_updated_at = base_model_instance.updated_at
+        sleep(0.1)
+        base_model_instance.save()
+        self.assertNotEqual(base_model_instance.updated_at, old_updated_at)
