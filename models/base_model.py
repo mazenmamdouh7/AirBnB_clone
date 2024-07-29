abe783351e5c53af8edabbde5888092840f95901
root <mazenmamdouh7>
2024-07-28 17:01:30 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,10 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
 
         else:
             self.id = str(uuid4())
