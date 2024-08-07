14b099cb3b3ecc81866a182f3a89ca29125836c2
root <mazenmamdouh7>
2024-08-07 00:54:36 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -50,7 +50,14 @@ class BaseModel:
 
 
 if __name__ == "__main__":
-    base_model_instance = BaseModel()
-    print(base_model_instance)
-    base_model_instance.save()
-    print(base_model_instance.to_dict())
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
