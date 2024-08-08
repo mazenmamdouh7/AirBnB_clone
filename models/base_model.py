f54cab3053394ca82695652c57dd1d6a9e576c53
root <mazenmamdouh7>
2024-08-07 23:26:45 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,11 @@
 class BaseModel that defines all
 common attributes/methods for other classes
 """
-import uuid
+from uuid import uuid4
 from datetime import datetime
 from models import storage
 
 class BaseModel:
-    def save(self):
-        """Update the updated_at attribute with the current datetime"""
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
-
     def __init__(self, *args, **kwargs):
         """Initialize the instance"""
         if kwargs:
@@ -27,6 +21,12 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance"""
